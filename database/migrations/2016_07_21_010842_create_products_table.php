e31d223f94efa4a4d3dc83545c5d052700085cb7
Justin Christenson <info@justinc.me>
2016-07-21 10:21:00 -0700

    FCV20 -Add products table migration

--- database/migrations/2016_07_21_010842_create_products_table.php
+++ database/migrations/2016_07_21_010842_create_products_table.php
@@ -15,8 +15,37 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function(Blueprint $table) {
             $table->increments('id');
-            $table->timestamp('active_on')->nullable();
-            $table->timestamp('expires_on')->nullable();
+            $table->string('name');
+            $table->text('short_desc');
+            $table->text('long_desc');
+            $table->string('sku')
+                ->unique();
+            $table->decimal('unit_price', 19, 2)
+                ->nullable();
+            $table->decimal('sale_price', 19, 2)
+                ->nullable();
+            $table->decimal('weight', 19, 2);
+            $table->integer('manufacturer_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('manufacturer_id')
+                ->references('id')
+                ->on('manufacturers')
+                ->onDelete('cascade');
+            $table->integer('category_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+            $table->integer('units_sold')
+                ->nullable();
+            $table->integer('units_available');
+            $table->timestamp('active_on')
+                ->nullable();
+            $table->timestamp('expires_on')
+                ->nullable();
             $table->timestamps();
         });
     }
