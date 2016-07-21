f67b4cc45c59faa9a6ad0e1c1bc8bdb2ed4bf70e
Justin Christenson <info@justinc.me>
2016-07-20 18:40:44 -0700

    FCV20 - Completed User table migration Added methods to User model

--- app/Core/User.php
+++ app/Core/User.php
@@ -11,8 +11,9 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+    protected $guarded = [
+        'role_id',
+        'active'
     ];
 
     /**
@@ -21,6 +22,27 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    /**
+     * Determine if user is registered.
+     *
+     * @return bool
+     */
+    public function isRegisteredUser()
+    {
+        return ($this->role_id > 1) ? true : false;
+    }
+
+    /**
+     * Determine if this is a temporary (unregistered) user.
+     *
+     * @return bool
+     */
+    public function isTempUser()
+    {
+        return (1 === $this->role_id) ? true : false;
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('email')
+                ->unique();
+            $table->string('secondary_email')->nullable();
             $table->string('password');
+            $table->integer('role_id')
+                ->unsigned()
+                ->default(1);
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+            $table->boolean('active')->default('true');
             $table->rememberToken();
             $table->timestamps();
         });
