9a2111942f8e588ad3f2ae9502ce9da2bac655b2
Justin Christenson <info@justinc.me>
2016-07-21 18:14:13 -0700

    FCV20 - Created product images table

--- database/migrations/2016_07_21_011147_create_product_images_table.php
+++ database/migrations/2016_07_21_011147_create_product_images_table.php
@@ -14,7 +14,19 @@ class CreateProductImagesTable extends Migration
     public function up()
     {
         Schema::create('product_images', function(Blueprint $table) {
-
+            $table->increments('id');
+            $table->integer('product_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+            $table->string('full_path');
+            $table->string('thumb_path');
+            $table->integer('sequence')
+                ->nullable();
+            $table->timestamps();
         });
     }
 
