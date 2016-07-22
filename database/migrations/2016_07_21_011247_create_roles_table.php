3d8cb78e08338d6e8d45fa0b52f7c9c05a6d5954
Justin Christenson <info@justinc.me>
2016-07-21 18:24:24 -0700

    FCV20 - Created roles table

--- database/migrations/2016_07_21_011247_create_roles_table.php
+++ database/migrations/2016_07_21_011247_create_roles_table.php
@@ -14,7 +14,8 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function(Blueprint $table) {
-
+            $table->increments('id');
+            $table->string('name');
         });
     }
 
