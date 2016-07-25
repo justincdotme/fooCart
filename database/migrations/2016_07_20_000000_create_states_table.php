2c1aa4573b6d33b5d25a13bc75e01b17c8da9add
Justin Christenson <info@justinc.me>
2016-07-24 18:30:37 -0700

    FCV2-20 Fixed missing timestamp on states table

--- database/migrations/2016_07_20_000000_create_states_table.php
+++ database/migrations/2016_07_20_000000_create_states_table.php
@@ -17,6 +17,7 @@ class CreateStatesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->char('abbreviation', 2);
+            $table->timestamps();
         });
     }
 
