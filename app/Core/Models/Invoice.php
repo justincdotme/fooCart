7234ef6fdf6a3c7c2aec087ae28176692d6c3908
Justin Christenson <info@justinc.me>
2016-07-26 19:44:16 -0700

    FCV2-23 Added invoice model Added tests for invoice model

--- app/Core/Models/Invoice.php
+++ app/Core/Models/Invoice.php
@@ -21,13 +21,29 @@ class Invoice extends Model
     protected $hidden = [];
 
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = [
+        'invoiceItems'
+    ];
+
+    /**
      * Define the relationship to invoice items.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function invoiceItems()
     {
-        return $this->hasMany('fooCart\Core\InvoiceItem');
+        return $this->hasMany('fooCart\Core\Models\InvoiceItem');
     }
 
     /**
@@ -37,7 +53,7 @@ class Invoice extends Model
      */
     public function shipments()
     {
-        return $this->hasMany('fooCart\Core\Shipment');
+        return $this->hasMany('fooCart\Core\Models\Shipment');
     }
 
     /**
@@ -47,7 +63,7 @@ class Invoice extends Model
      */
     public function user()
     {
-        return $this->belongsTo('fooCart\Core\User');
+        return $this->belongsTo('fooCart\Core\Models\User');
     }
 
     /**
@@ -57,7 +73,7 @@ class Invoice extends Model
      */
     public function promotion()
     {
-        return $this->belongsTo('fooCart\Core\PromoCode', 'promo_code_id');
+        return $this->belongsTo('fooCart\Core\Models\PromoCode', 'promo_code_id');
     }
 
     /**
@@ -67,6 +83,69 @@ class Invoice extends Model
      */
     public function bankcard()
     {
-        return $this->belongsTo('fooCart\Core\Bankcard', 'promo_code_id');
+        return $this->belongsTo('fooCart\Core\Models\Bankcard', 'promo_code_id');
+    }
+
+    /**
+     * Get the total cost of the invoice.
+     *
+     * @return float
+     */
+    public function getPriceTotal()
+    {
+        return $this->getPriceSubtotal() - $this->getPromotionTotal();
+    }
+
+    /**
+     * Get the subtotal of all invoice items.
+     *
+     * @return float
+     */
+    public function getPriceSubtotal()
+    {
+        //array_reduce the $invoiceItems->getPriceTotal() for each item
+        $total = array_reduce($this->invoiceItems->all(), function($carry, $item)
+        {
+            $carry += $item->getPriceTotal();
+            return $carry;
+        }, 0);
+        return ($total < 0) ? 0 : $total;
+    }
+
+    /**
+     * Get the total promotion amount for the invoice.
+     *
+     * @return float
+     */
+    public function getPromotionTotal()
+    {
+        $promotionAmount = 0.00;
+        if (!is_null($this->promo_code_id)) {
+            $promotion = $this->promotion()->first();
+            if ('percentage' === $promotion->type) {
+                $promotionAmount = ($this->getPriceSubtotal() * $promotion->discount_percent);
+            } else if ('amount' === $promotion->type) {
+                $promotionAmount = $promotion->discount_amount;
+            }
+        }
+
+        return $promotionAmount;
+    }
+
+    /**
+     * Calculate the tax total for the invoice.
+     *
+     * @return float
+     */
+    public function getTaxTotal()
+    {
+        //array_reduce All $this->invoiceItem()->all()->getTaxTotal()
+        //To calculate the tax total for the invoice
+        $total = array_reduce($this->invoiceItems()->all(), function($carry, $item)
+        {
+            $carry += $item->getTaxTotal();
+            return $carry;
+        }, 0);
+        return $total;
     }
 }
