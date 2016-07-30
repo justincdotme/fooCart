6d985dfd3883677e8e5f0634095518aad1df4fd5
Justin Christenson <info@justinc.me>
2016-07-29 18:48:04 -0700

    FCV2-0 Removed test route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,11 +1,5 @@
 <?php
 
-use fooCart\Core\Models\Invoice;
-
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('test', function () {
-    $invoice = Invoice::find(1);
-    return Invoice::find(1)->getPriceTotal();
 });
