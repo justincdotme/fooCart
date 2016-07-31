524c9049cbe938db9d8d33d93ed1268bb689b3ac
Justin Christenson <info@justinc.me>
2016-07-30 20:00:52 -0700

    FCV2-24 Added category model Added manufacturer model

--- app/Core/Models/Category.php
+++ app/Core/Models/Category.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
 }
