62e4c48973a75be0a7fffa0b387ce1187ac96f5b
Justin Christenson <info@justinc.me>
2016-07-31 16:54:29 -0700

    FCV2-24 Added product image table seeder Added all seeders to database seeder class Added product images Tweaked eager loads for product model Killed unused ProductCategory model

--- app/Core/Models/Product.php
+++ app/Core/Models/Product.php
@@ -22,6 +22,17 @@ class Product extends Model
     protected $hidden = [];
 
     /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = [
+        'images',
+        'manufacturer',
+        'categories'
+    ];
+
+    /**
      * Define the relationship to the manufacturer.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -31,10 +42,36 @@ class Product extends Model
         return $this->belongsTo('fooCart\Core\Models\Manufacturer');
     }
 
+    /**
+     * Define the relationship to the product shipping options.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function shippingOptions()
     {
         return $this->belongsToMany('fooCart\Core\Models\ShippingOption', 'product_shipping_options')
-            ->withPivot('shipping_cost', 'created_at', 'updated_at');
+            ->withPivot('shipping_cost', 'created_at', 'updated_at')
+            ->withTimestamps();
+    }
+
+    /**
+     * Define the relationship to the product images.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function images()
+    {
+        return $this->hasMany('fooCart\Core\Models\ProductImage');
+    }
+
+    /**
+     * Define the relationship to the product categories.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories()
+    {
+        return $this->belongsToMany('fooCart\Core\Models\Category', 'product_categories');
     }
 
     /**
