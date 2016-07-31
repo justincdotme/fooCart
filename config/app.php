0abbd00667bb7d8d19ef4f913565b6488a5bf092
Justin Christenson <info@justinc.me>
2016-07-30 20:34:17 -0700

    FCV2-6 Updated default app URL

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://foocart.demos.justinc.me'),
 
     /*
     |--------------------------------------------------------------------------
